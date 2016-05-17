bccfe2cf50bc2894bac2e405e5ab26b52836cdbb
treanorv <treanorv@gmail.com>
2016-05-17 18:33:44 +0100

    Initial Upload

--- Loopy/Form1.Designer.cs
+++ Loopy/Form1.Designer.cs
@@ -29,11 +29,155 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.loopyContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripRestore = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.loopyContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(138, 145);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(85, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(138, 174);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(85, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 257);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(257, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(58, 17);
+            this.statusBar.Text = "status bar";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.loopyContextMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // loopyContextMenu
+            // 
+            this.loopyContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripRestore,
+            this.toolStripStart,
+            this.toolStripStop,
+            this.toolStripExit});
+            this.loopyContextMenu.Name = "loopyContextMenu";
+            this.loopyContextMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.loopyContextMenu.Size = new System.Drawing.Size(114, 92);
+            // 
+            // toolStripRestore
+            // 
+            this.toolStripRestore.Name = "toolStripRestore";
+            this.toolStripRestore.Size = new System.Drawing.Size(113, 22);
+            this.toolStripRestore.Text = "Restore";
+            this.toolStripRestore.Click += new System.EventHandler(this.toolStripRestore_Click);
+            // 
+            // toolStripStart
+            // 
+            this.toolStripStart.Name = "toolStripStart";
+            this.toolStripStart.Size = new System.Drawing.Size(113, 22);
+            this.toolStripStart.Text = "Start";
+            this.toolStripStart.Click += new System.EventHandler(this.toolStripStart_Click);
+            // 
+            // toolStripStop
+            // 
+            this.toolStripStop.Name = "toolStripStop";
+            this.toolStripStop.Size = new System.Drawing.Size(113, 22);
+            this.toolStripStop.Text = "Stop";
+            this.toolStripStop.Click += new System.EventHandler(this.toolStripStop_Click);
+            // 
+            // toolStripExit
+            // 
+            this.toolStripExit.Name = "toolStripExit";
+            this.toolStripExit.Size = new System.Drawing.Size(113, 22);
+            this.toolStripExit.Text = "Exit";
+            this.toolStripExit.Click += new System.EventHandler(this.toolStripExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(69, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "no-paper.co.uk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = global::Loopy.Properties.Resources.wav_icon;
+            this.ClientSize = new System.Drawing.Size(257, 279);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(273, 317);
+            this.MinimumSize = new System.Drawing.Size(273, 317);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "White Noise";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.loopyContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBar;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip loopyContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripRestore;
+        private System.Windows.Forms.ToolStripMenuItem toolStripStart;
+        private System.Windows.Forms.ToolStripMenuItem toolStripStop;
+        private System.Windows.Forms.ToolStripMenuItem toolStripExit;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Loopy/Form1.cs
+++ Loopy/Form1.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace Loopy
 {
     public partial class Form1 : Form
     {
+        private SoundPlayer player;
+        private string path;
         public Form1()
         {
             InitializeComponent();
+            //player = new SoundPlayer();
+            player = new SoundPlayer(Loopy.Properties.Resources.loop_01);
+            path = Environment.SpecialFolder.UserProfile.ToString() + @"\Music";
+            path = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%") + @"\Music";
+            //player.SoundLocation = path + @"\loop-01.wav";
+            statusBar.Text = player.SoundLocation;
+            notifyIcon1.BalloonTipTitle = "Noise Loop";
+            notifyIcon1.BalloonTipText = "Right Click for Menu";            
+            notifyIcon1.ShowBalloonTip(1000);
+            WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            player.PlayLooping();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+        
+        private void toolStripExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void toolStripStart_Click(object sender, EventArgs e)
+        {
+            player.PlayLooping();
+        }
+
+        private void toolStripStop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void toolStripRestore_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
         }
     }
 }
